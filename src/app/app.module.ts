9c7a6927723761b2133d53cfcb65262b961ae5a4
Victor Burghi <victor.burghi@gmail.com>
2023-02-27 10:46:05 -0300

    testes iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimengModule } from './common/primeng.module';
+import { FontawesomeModule } from './common/fontawesome.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    PrimengModule,
+    FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/common/fontawesome.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+  ], 
+  exports: [
+    FontAwesomeModule
+  ]
+})
+export class FontawesomeModule { }
